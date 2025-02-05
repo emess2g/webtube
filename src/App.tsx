820b4e51557d8bb682891b8b079757227f3b3bec
emess2g <essuonemma84gmail.com>
2025-02-05 11:52:52 +0000

    category pill

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import PageHeader from "./layout/PageHeader";
+import { CategoryPills} from "./components/CategoryPIlls"
+import { categories } from "./data/home";
+import { useState } from "react";
 
 export default function App() {
+  const [selectedCategory, setSelectedCategory] = useState(categories[0])
    return (
     <div className="min-h-screen flex flex-col">
       <PageHeader/>
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="overflow-x-hidden px-8 pb-4">
+          {/* video */}
+          <div className="sticky top-0 bg-white z-10 pb-4">
+            <CategoryPills 
+            selectedCategory={selectedCategory}
+            onSelect={setSelectedCategory} 
+            categories={categories}/>
+          </div>
+          <div className="grid gap-4 grid-cols-[repeat(auto-fill,minmax(300px)"></div>
+        </div>
+      </div>
     </div>
    )
 }
