af678121b701d71e751a6a7437a98367cb00ffd1
emess2g <essuonemma84gmail.com>
2025-02-09 02:43:16 +0000

    sidebar

---
+++ src/layout/Sidebar.tsx
@@ -0,0 +1,70 @@
+import { Clapperboard, Home, Library, Repeat } from "lucide-react";
+import { Children, ElementType, ReactNode } from "react";
+import { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
+
+export function Sidebar() {
+    return (
+        <>
+          <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+       <SmallSidebarItem Icon={Home} title="Home" url="/" />
+       <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+       <SmallSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
+       <SmallSidebarItem Icon={Library} title="Library" url="/library" />
+       </aside>
+        <aside className="w-56 lg:sticky absolute overflow-y-auto scrollbar-hidden pb-4 px-2 gap-2 flex flex-col">
+            <LargeSidebarSection visibleItemCount={1}>
+                <LargeSidebarItem isActive Icon={Home} title="Home" url="/"/>
+                <LargeSidebarItem isActive Icon={Home} title="Home" url="/"/>
+            </LargeSidebarSection>
+        </aside>
+        </>
+    )
+    
+}
+
+type SmallSidebarItemProps = {
+    Icon: ElementType
+    title: string
+    url: string
+}
+
+function SmallSidebarItem({ Icon, title, url}: 
+    SmallSidebarItemProps){
+    return <a   href={url} className={twMerge(buttonStyles({ variant: "ghost" }), "py-4 px-1 flex flex-col items-center rounded-lg gap-1")}>
+        <Icon className="w-6 h-6"/>
+        <div className="text-sm">{title}</div>
+    </a>
+}
+
+type LargeSidebarSectionProps = {
+    children: ReactNode
+    title?: string
+    visibleItemCount?: number
+}
+
+function LargeSidebarSection({ children, title, visibleItemCount = Number.POSITIVE_INFINITY}: LargeSidebarSectionProps){
+    const childrenArray = Children.toArray(children).flat()
+    const visibleChildren = childrenArray.slice(0, visibleItemCount)
+    return <div>
+        {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
+        {visibleChildren}
+    </div>
+}
+
+type LargeSidebarItemProps = {
+    Icon: ElementType
+    title: string
+    url: string
+    isActive?: boolean
+}
+
+function LargeSidebarItem({ Icon, title, url, isActive = false}: LargeSidebarItemProps){
+   return <a href={url} className={twMerge(buttonStyles({variant:"ghost"}), 
+   `w-full flex items-center rounded-lg gap-4 p-3 ${ isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : undefined}`)}>
+    <Icon className="w-6 h-6"/>
+    <div className="whitespace-nowrap overflow-hidden text-ellipsis"> 
+        {title}
+    </div>
+   </a>
+}
